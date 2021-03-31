8348b9093de3d34d6eea4554b842720b33d011c6
Andrés K. Aquin <andruuus123@gmail.com>
2021-03-31 19:43:58 -0400

    Agregar archivos de proyecto.

---
+++ Buzón/Buzón.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andru
+ * Fecha: 31/3/2021
+ * Hora: 19:30
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Buzón
+{
+	/// <summary>
+	/// Description of Buzón.
+	/// </summary>
+	public class Buzón
+	{
+		public Buzón()
+		{
+		}
+	}
+}
---
+++ Buzón/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andru
+ * Fecha: 31/3/2021
+ * Hora: 19:28
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Buzón
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
